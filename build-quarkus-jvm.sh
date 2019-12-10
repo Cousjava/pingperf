14a8faa6136529cd4233ea5b355f8e3b7b2d1608
Jonathan Coustick <jonathan.coustick@payara.fish>
2019-12-10 15:41:01 +0000

    Added Dockerfiles to run openliberty and Payara Micro

--- build-quarkus-jvm.sh
+++ build-quarkus-jvm.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-mvn package  && docker build -f Dockerfile.quarkus-jvm -t pingperf-quarkus-jvm .
+mvn package  && sudo docker build -f Dockerfile.quarkus-jvm -t pingperf-quarkus-jvm .
+mvn package -Ppayara-micro && sudo docker build -f Dockerfile.payara-micro -t pingperf-payara-micro .
+mvn package -Popenliberty && sudo docker build -f Dockerfile.openliberty -t pingperf-openliberty .
---
+++ run-payaramicro.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo docker run -ti --rm -p 8080:8080 --network host pingperf-payara-micro
